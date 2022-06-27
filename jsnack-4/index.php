39efada28bdfd6e882aa7b7450cfa06697039bf9
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-06-27 18:20:21 +0200

    finito

---
+++ jsnack-4/index.php
@@ -0,0 +1,32 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body><?php
+
+
+
+    $numberArr = [];
+
+    while (count($numberArr) < 40) {
+        $number = rand(1, 40);
+        if (! in_array($number, $numberArr)) {
+            $numberArr[] = $number;
+            echo "$number <br/>";
+        }
+    };
+
+
+
+?>
+</body>
+</html>
